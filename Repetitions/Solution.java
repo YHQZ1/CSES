f81f8ce51d458bf0449e3d915b2dfb212b56596a
Uttkarsh <rupareluttkarsh2309@gmail.com>
2026-06-02 10:56:20 +0530

    solve: Repetitions

---
+++ Repetitions/Solution.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Solution {
+  private static int repetitions(String dna) {
+    int max = 0;
+    int curr = 0;
+    for (int i = 1; i < dna.length(); i++) {
+      if (dna.charAt(i) == dna.charAt(i - 1))
+        curr++;
+      else
+        curr = 1;
+      max = Math.max(curr, max);
+    }
+    return max;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String dna = sc.nextLine();
+
+    int result = repetitions(dna);
+    System.out.println(result);
+
+    sc.close();
+  }
+}
