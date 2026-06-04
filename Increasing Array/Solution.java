cd6cc956b569469337620d0e5b629a8dfe657de7
Uttkarsh Ruparel <157672792+YHQZ1@users.noreply.github.com>
2026-06-04 09:57:12 +0530

    solve: Increasing Array

--- Increasing Array/Solution.java
+++ Increasing Array/Solution.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
-
+ 
 public class Solution {
   private static long increasingArray(int[] arr) {
     long moves = 0;
-
+ 
     for (int i = 1; i < arr.length; i++) {
       if (arr[i] < arr[i - 1]) {
         int difference = arr[i - 1] - arr[i];
@@ -13,20 +13,20 @@ public class Solution {
     }
     return moves;
   }
-
+ 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-
+ 
     int size = sc.nextInt();
     int[] arr = new int[size];
-
+ 
     for (int i = 0; i < arr.length; i++) {
       arr[i] = sc.nextInt();
     }
-
+ 
     long result = increasingArray(arr);
     System.out.println(result);
-
+ 
     sc.close();
   }
 }
